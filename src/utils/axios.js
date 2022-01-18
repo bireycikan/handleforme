5639b0fbf6ad1a3b910290406fde54a00c0b0cde
Birey Cikan <bireycikan@gmail.com>
2022-01-18 18:20:51 +0300

    move axios base url to .env file

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/"
+  baseURL: import.meta.env.VITE_AXIOS_BASE_URL
 })
 
 axiosInstance.defaults.withCredentials = true;
